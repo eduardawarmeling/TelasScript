773e77b65d4375a153fd3674cbca0b15486cbe28
eduardawarmeling <eduarda_medeiros@estudante.sesisenai.org.br>
2023-06-15 12:14:29 -0300

    aula

---
+++ perfil_usuario/perfil.js
@@ -0,0 +1,12 @@
+function showImagePreview(event) {
+  const input = event.target;
+  const file = input.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function(e) {
+    const imagePreview = document.getElementById('image-preview');
+    imagePreview.src = e.target.result;
+  };
+
+  reader.readAsDataURL(file);
+}
